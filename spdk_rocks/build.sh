aa75ecb8695c1646ba3db741e61f89b800eb6f72
Tomoya Suzuki <t3suzuki@ucsd.edu>
2023-10-23 22:59:57 +0000

    up

---
+++ spdk_rocks/build.sh
@@ -0,0 +1,2 @@
+#docker build -t spdk_rocksdb:1 . --no-cache=true
+docker build -t spdk_rocksdb:1 .
---
+++ spdk_rocks/run.sh
@@ -0,0 +1,1 @@
+docker run -it --name spdk_rocksdb-1 --privileged --security-opt seccomp=unconfined spdk_rocksdb:1
